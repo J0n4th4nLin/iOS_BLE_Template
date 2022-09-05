f6bc07734069bfec6ec7bc85ae9e213d0d05bde6
Jonathan <j0n4th4nlin@gmail.com>
2022-09-05 09:55:51 +0800

    Initial Commit

---
+++ iOS_BLE/iOS_BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BLEApp.swift
+//  iOS_BLE
+//
+//  Created by Jonathan Lin on 2022/9/5.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
